5b2364af10c0f08d09a55949d5b4463c5bde82e9
思博 董 <fc1466727732@163.com>
2021-04-17 20:16:11 +0800

    完善路由

--- src/router/index.js
+++ src/router/index.js
@@ -12,34 +12,41 @@ const routes = [{
   component: () =>
     import ( /* webpackChunkName: "about" */ '../views/Index.vue'),
   children: [{
-    path: 'index',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/Index.vue')
-  }, {
-    path: 'us',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/Us.vue')
-  }, {
-    path: 'about',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/About.vue')
-  }, {
-    path: 'school',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/School.vue')
-  }, {
-    path: 'industry',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/Industry.vue')
-  }, {
-    path: 'design',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/Design.vue')
-  }, {
-    path: 'index',
-    component: () =>
-      import ( /* webpackChunkName: "about" */ '../views/pages/Index.vue')
-  }, ]
+      path: 'index',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/Index.vue')
+    },
+    {
+      path: 'us',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/Us.vue')
+    },
+    {
+      path: 'about',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/About.vue')
+    },
+    {
+      path: 'school',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/School.vue')
+    },
+    {
+      path: 'industry',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/Industry.vue')
+    },
+    {
+      path: 'design',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/Design.vue')
+    },
+    {
+      path: 'index',
+      component: () =>
+        import ( /* webpackChunkName: "about" */ '../views/pages/Index.vue')
+    },
+  ]
 }]
 
 const router = new VueRouter({
